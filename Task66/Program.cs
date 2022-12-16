c9be779b7f5053e008c2bf82567da10bb6466b5e
Engineer <arsenya.suvorov@mail.ru>
2022-12-16 11:46:54 +0400

    start

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int SumBetweenNumsRec(int m, int n)
+{
+    if (m > n)
+        return SumBetweenNumsRec(m - 1, n) + m;
+    else if (m < n)
+        return SumBetweenNumsRec(m, n - 1) + n;
+    else return n;
+}
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма натуральных чисел между {m} и {n} равна {SumBetweenNumsRec(m, n)}");
