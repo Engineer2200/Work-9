b264f0f314c6bdef8aba96d4a87ab0ec9a9d5a18
Engineer <arsenya.suvorov@mail.ru>
2022-12-16 11:48:40 +0400

    start

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Ackermann( int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if ((m > 0) && (n == 0))
+        return Ackermann(m - 1, 1);
+    else if ((m > 0) && (n > 0))
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    else
+        return n + 1;
+}
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"A({m}, {n}) = {Ackermann(m, n)}");
